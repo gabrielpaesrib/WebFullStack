fc6a618b40a30bd522c4318f1b872cf4294c887c
Gabriel <gabrielpaesrib@hotmail.com>
2018-05-25 11:41:50 -0300

    2 Update / Adicionado Galeria e Redes Sociais agora são clicaveis

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@
 $(document).ready(function(){
     // DEBUGAR //console.log('Funcionou!')
     $('#txtNome').blur(function(){
-     alert($(this).val());
+    	var nome = $(this).val();
     });
 });
